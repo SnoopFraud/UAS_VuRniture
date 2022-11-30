6fa29425240f7b14eb2791e5c6e3270e24ee20e7
M.S.R <radityoadit9@gmail.com>
2022-11-30 10:11:08 +0700

    Added Movement for Player

---
+++ Assets/Script/PlayerWalk.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour
+{
+    public int _DistanceRay = 20;
+    private RaycastHit _hit;
+    public int _playerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out _hit, _DistanceRay))
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                if(_hit.transform.CompareTag("Furniture"))
+                {
+                    Debug.Log("NO MOVE");
+                }
+                else
+                {
+                    transform.position
+                    = transform.position + Camera.main.transform.forward * _playerSpeed * Time.deltaTime;
+                }
+            }
+            
+        }
+        
+    }
+}
